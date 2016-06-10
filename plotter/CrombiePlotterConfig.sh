b1aa9f9ddc5d73220173362c2460c959f0c8be2f
Daniel Abercrombie <dabercro@mit.edu>
2016-06-10 13:47:27 +0200

    Config

--- plotter/CrombiePlotterConfig.sh
+++ plotter/CrombiePlotterConfig.sh
@@ -1,12 +1,13 @@
 source ../slimmer/CrombieSlimmingConfig.sh
 
-export CrombieMCConfig=MCConfig.txt
+#export CrombieMCConfig=MCConfig.txt
+export CrombieMCConfig=MCTT.txt
 export CrombieSignalConfig=SignalConfig.txt
 export CrombieExcept_trailing=MCTrailing.txt
 export CrombieExcept_mass=MCMass.txt
 export CrombieLuminosity=814.56
-export CrombieInFilesDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_160610
-export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE
+export CrombieInFilesDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_160609
+export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE\_80X
 export CrombieOutLimitTreeDir=limits/$CROMBIEDATE
 
 export CrombieCutsFile=cuts.py
