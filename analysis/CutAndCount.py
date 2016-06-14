55d789a495d08cc91da1e30b452651679e8ed2c8
dabercro <dabercro@mit.edu>
2016-06-14 14:19:35 +0200

    Adding cut and count format.

--- analysis/CutAndCount.py
+++ analysis/CutAndCount.py
@@ -12,11 +12,13 @@ from ROOT import TFile
 
 data = TFile(env.get('CrombieInFilesDir') + '/wscale_Data.root')
 mc   = TFile(env.get('CrombieInFilesDir') + '/res/wscale_TTJets.root')
+back = TFile(env.get('CrombieInFilesDir') + '/nonres/wscale_TTJets.root')
 
 factor = str(670.3 / mc.htotal.GetBinContent(1) * float(os.environ['CrombieLuminosity']))
 
 dataTree = data.events
 mcTree = mc.events
+backTree = back.events
 
 plotter.SetDefaultExpr('fatjet1DRLooseB')
 plotter.AddTreeWeight(dataTree,cuts.cut('semilep','full'))
@@ -30,6 +32,12 @@ plotter.AddTreeWeight(mc.events,'(' + cuts.cut('semilep','full_tau21') + ') * (m
 
 plotter.AddTreeWeight(data.events,cuts.cut('semilep','full_tau21_massp'))
 plotter.AddTreeWeight(mc.events,'(' + cuts.cut('semilep','full_tau21_massp') + ') * (mcFactors * '+ factor +')')
+
+plotter.AddTreeWeight(backTree,'(' + cuts.cut('semilep','full') + ') * (mcFactors * '+ factor +')')
+plotter.AddTreeWeight(backTree,'(' + cuts.cut('semilep','full_massp') + ') * (mcFactors * '+ factor +')')
+plotter.AddTreeWeight(backTree,'(' + cuts.cut('semilep','full_tau21') + ') * (mcFactors * '+ factor +')')
+plotter.AddTreeWeight(backTree,'(' + cuts.cut('semilep','full_tau21_massp') + ') * (mcFactors * '+ factor +')')
+
 
 hists = plotter.MakeHists(1,0,10)
 
@@ -49,14 +57,14 @@ data.Close()
 mc.Close()
 
 for index in range(4):
-    data  = ints[index*2]
-    dataE = errs[index*2]
+    data  = ints[index*2] - ints[index + 8]
+    dataE = m.sqrt(m.pow(errs[index*2],2) + m.pow(errs[index + 8],2))
 
     mc  = ints[index*2 + 1]
     mcE = errs[index*2 + 1]
 
     print ('Norm ratio:')
-    print (data/mc * ints[1]/ints[0])
+    print (data/mc * ints[1]/(ints[0] - ints[8]))
 
     print ('Norm uncertainty:')
-    print (m.sqrt(m.pow(dataE/mc,2) + m.pow(data/mc/mc*mcE,2)) * ints[1]/ints[0])
+    print (m.sqrt(m.pow(dataE/mc,2) + m.pow(data/mc/mc*mcE,2)) * ints[1]/(ints[0] - ints[8]))
