bd899faa79b7c9f644b48ecf6e2abb2920402d9a
dabercro <dabercro@mit.edu>
2016-06-21 15:32:45 +0200

    laptop config.

--- slimmer/CrombieSlimmingConfig.sh
+++ slimmer/CrombieSlimmingConfig.sh
@@ -7,17 +7,16 @@ export CrombieNLocalProcs=6
 export CrombieEosDir=80X.txt
 
 export CrombieFileBase=wscale
-#export CrombieEosDir=/store/user/zdemirag/setup80x/Nero/zey_base
-#export CrombieRegDir=/afs/cern.ch/work/d/dabercro/eoscms/cms$CrombieEosDir
 export CrombieTempDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Temp
-export CrombieFullDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Full_160621
-export CrombieSkimDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_$CROMBIEDATE\_newPU
+export CrombieFullDir=/Users/dabercro/GradSchool/Summer16/Full_160621
+export CrombieSkimDir=/Users/dabercro/GradSchool/Summer16/Skim_160621
 export CrombieDirList=TopStudy.txt
 
 export CrombieSlimmerScript=runSlimmer.py
 export CrombieJobScriptList=CrombieJobScriptList.txt
 export CrombieCheckerScript=$CROMBIEPATH/scripts/CrombieTreeFinder.py
 
-#export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274240_13TeV_PromptReco_Collisions16_JSON.txt
-export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt
-#export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt
+
+#export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-274240_13TeV_PromptReco_Collisions16_JSON.txt
+export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt
+#export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt
