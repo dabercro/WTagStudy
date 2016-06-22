0d0b6e9e6aed14f319bac763f5302a31a996b3e9
Daniel Abercrombie <dabercro@mit.edu>
2016-06-22 17:48:25 +0200

    Will want the correct correction.

--- slimmer/applyCorrectionsW.py
+++ slimmer/applyCorrectionsW.py
@@ -2,8 +2,8 @@
 
 from applyCorrections import *
 
-addCorr('ewk_w','genBosPt','abs(genBosPdgId) == 24','files/puWeight890pb.root','hPU')
+addCorr('ewk_w','genBosPt','abs(genBosPdgId) == 24','files/uncertainties_EWK_24bins.root',['EWKcorr/W','WJets_012j_NLO/nominal'])
+addCorr('kfactor_w','genBosPt','abs(genBosPdgId) == 24','files/uncertainties_EWK_24bins.root',['WJets_012j_NLO/nominal','WJets_LO/inv_pt'])
 
 if __name__ == "__main__":
     RunOnDirectory(applicator)
-
