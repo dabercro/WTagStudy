76a407b8d2e430f5e776ca9799280d8698a36082
dabercro <dabercro@mit.edu>
2016-06-24 13:08:53 +0200

    Merging looks okay so far.

--- plotter/CrombiePlotterConfig.sh
+++ plotter/CrombiePlotterConfig.sh
@@ -1,10 +1,13 @@
 source ../slimmer/CrombieSlimmingConfig.sh
 
 export CrombieMCConfig=MCConfig.txt
-export CrombieExcept_hadronic=MCTTJets.txt
-export CrombieLuminosity=2071.44
-export CrombieInFilesDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_160623
-export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE
+#export CrombieMCConfig=MCTT.txt
+#export CrombieMCConfig=MCTest.txt
+#export CrombieSignalConfig=SignalConfig.txt
+#export CrombieLuminosity=2071.44
+export CrombieLuminosity=814.56
+export CrombieInFilesDir=../../Skim_LowLumi
+export CrombieOutPlotDir=plots/$CROMBIEDATE\_LowLumi_both
 export CrombieOutLimitTreeDir=limits/$CROMBIEDATE
 
-export CrombieCutsFile=cutsHad.py
+export CrombieCutsFile=cuts.py
--- slimmer/CrombieSlimmingConfig.sh
+++ slimmer/CrombieSlimmingConfig.sh
@@ -2,23 +2,21 @@ export CrombieFilesPerJob=8
 export CrombieNBatchProcs=1
 export CrombieQueue=8nh
 
-export CrombieNLocalProcs=6
+export CrombieNLocalProcs=3
 
 export CrombieEosDir=80X.txt
-#export CrombieEosDir=/store/user/zdemirag/setup80x/Nero/Data/zey_base
 
 export CrombieFileBase=wscale
-export CrombieRegDir=/afs/cern.ch/work/d/dabercro/eoscms/cms$CrombieEosDir
 export CrombieTempDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Temp
-export CrombieFullDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Full_$CROMBIEDATE
-export CrombieSkimDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_$CROMBIEDATE
+export CrombieFullDir=/Users/dabercro/GradSchool/Summer16/Full_160621
+export CrombieSkimDir=/Users/dabercro/GradSchool/Summer16/Skim_LowLumi
 export CrombieDirList=TopStudy.txt
 
 export CrombieSlimmerScript=runSlimmer.py
 export CrombieJobScriptList=CrombieJobScriptList.txt
 export CrombieCheckerScript=$CROMBIEPATH/scripts/CrombieTreeFinder.py
 
-#export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt
-#export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274240_13TeV_PromptReco_Collisions16_JSON.txt
-export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt
-#export CrombieGoodRuns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt
+#export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt
+export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-274240_13TeV_PromptReco_Collisions16_JSON.txt
+#export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt
+#export CrombieGoodRuns=/Users/dabercro/GradSchool/Summer16/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt
