740515a4fc2ef333142998afad873087b71770c0
Daniel Abercrombie <dabercro@mit.edu>
2016-07-04 18:09:35 +0200

    Tag this version, probably.

--- plotter/CrombiePlotterConfig.sh
+++ plotter/CrombiePlotterConfig.sh
@@ -8,10 +8,10 @@ export CrombieMCConfig=MCLongName.txt
 export CrombieLuminosity=2596.9
 #export CrombieLuminosity=2071.44
 export CrombieInFilesDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_160704
-export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE\_fin
+export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE
 
 export CrombieOutLimitTreeDir=limits/$CROMBIEDATE
 
 #export CrombieCutsFile=cutsHad.py
-#export CrombieCutsFile=cuts.py
-export CrombieCutsFile=cutsNew.py
+export CrombieCutsFile=cuts.py
+#export CrombieCutsFile=cutsNew.py
