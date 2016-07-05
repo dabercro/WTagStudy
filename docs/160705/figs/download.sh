6d54736c0684bd03b1ce9726893d28a927b5ddfd
dabercro <dabercro@mit.edu>
2016-07-05 12:16:22 +0200

    Documenting update.

---
+++ docs/160705/figs/download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dirs="old new small powheg herwig"
+images="semilep_full_fatjetPrunedM.pdf semilep_full_fatjettau21.pdf"
+
+for dir in $dirs
+do
+
+    url=http://dabercro.web.cern.ch/dabercro/plots/16070
+    if [ "$dir" = "small" ]
+    then
+        url=$url\5/
+    else
+        url=$url\4_$dir/
+    fi
+
+    for image in $images
+    do
+        echo $url$image
+        wget $url$image
+        mv $image $dir/$image
+    done
+
+done
