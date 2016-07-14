3a5717533641b4871c9d5d783d38dd77e01442b8
Daniel Abercrombie <dabercro@mit.edu>
2016-07-14 17:17:55 +0200

    Scale factor is changing. Check systematics...

--- plotter/CrombiePlotterConfig.sh
+++ plotter/CrombiePlotterConfig.sh
@@ -1,13 +1,14 @@
-source ../slimmer/CrombieSlimmingConfig.sh
-
 export CrombieMCConfig=MCBackground.txt
+export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE\_background
 #export CrombieMCConfig=MCTruthMatch.txt
+#export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE
+
+source ../slimmer/CrombieSlimmingConfig.sh
 #export CrombieMCConfig=MCHadronic.txt
 
 #export CrombieMCConfig=MCHadronic.txt
 export CrombieLuminosity=7647.6
 export CrombieInFilesDir=/afs/cern.ch/work/d/dabercro/public/Summer16/Skim_160714
-export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE
 
 export CrombieOutLimitTreeDir=limits/$CROMBIEDATE
 
