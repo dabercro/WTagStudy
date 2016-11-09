22ce8f029ff3cfb5c34188ad01512f9c8dd2e85a
Daniel Abercrombie <dabercro@mit.edu>
2016-11-09 14:31:29 +0100

    Some minor changes that I don't really remember.

--- slimmer/CrombieSlimmingConfig.sh
+++ slimmer/CrombieSlimmingConfig.sh
@@ -1,6 +1,6 @@
 export CrombieFilesPerJob=20
-export CrombieNBatchProcs=4
-export CrombieQueue=2nw4cores
+export CrombieNBatchProcs=1
+export CrombieQueue=8nh
 
 if [ "$(hostname)" = "dabercro-MacBookAir" ]
 then
@@ -9,14 +9,14 @@ else
     export CrombieNLocalProcs=6
 fi
 
-export CrombieEosDir=/store/user/dabercro/Nero/80X
+export CrombieEosDir=/store/user/dabercro
 export CrombieFileBase=wscale
 export CrombieRegDir=/afs/cern.ch/work/d/dabercro/eosuser/user/d/dabercro/80X
 #export CrombieFullDir=/tmp/dabercro/Full_$CROMBIEDATE
 export CrombieTempDir=/afs/cern.ch/work/d/dabercro/public/Fall16/Temp
 export CrombieFullDir=/afs/cern.ch/work/d/dabercro/public/Fall16/Full_$CROMBIEDATE
 export CrombieSkimDir=/afs/cern.ch/work/d/dabercro/public/Fall16/Skim_$CROMBIEDATE
-#export CrombieDirList=WScale.txt
+export CrombieDirList=VA.txt
 
 export CrombieSlimmerScript=runSlimmer.py
 export CrombieJobScriptList=JobScriptList.txt
